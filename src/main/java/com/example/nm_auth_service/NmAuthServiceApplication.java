bdbc4ec27a702f3ac1339456336305aea5252352
Anubhav Nath <anubhav.n@webknot.in>
2025-04-14 22:48:26 +0530

    initial commit for project setup

---
+++ src/main/java/com/example/nm_auth_service/NmAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.nm_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NmAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nm_auth_service/NmAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nm_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
